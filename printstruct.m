91b72d462753cb69179d600ac763d46dec0e05fd
Bob Spunt <bobspunt@gmail.com>
2017-08-30 19:52:16 -0700

    updated to allow passing cell arrays containing structures, which are printed iteratively

--- printstruct.m
+++ printstruct.m
@@ -1,65 +1,76 @@
 function pS = printstruct(S, varargin)
 % PRINTSTRUCT Recursively print hierarchical outline of structure contents
-% 
+%
 %  This is a minor adaptation of the File Exchange contribution "Structure
 %  outline" written by B. Roossien <roossien@ecn.nl> and available here:
 %  http://mathworks.com/matlabcentral/fileexchange/13500-structure-outline
 % __________________________________________________________________________
 %  USAGE: pS = printstruct(S, varargin)
-% 
+%
 %    IN:   S = structure variable to print
 %  OUT*:  pS = cell array containing printed structure
-%  
+%
 %   *If defined, result will NOT display in command window
 % __________________________________________________________________________
 %  OPTIONAL VARARGIN* [entered as 'name', value pairs]:
 %   *Run printstruct w/no arguments to see default values
-% 
-%   NLEVELS:        N levels to print. If negative, all levels printed. 
+%
+%   NLEVELS:        N levels to print. If negative, all levels printed.
 %   NINDENT:        number of tab indents for each line of printed struct
 %   STRUCTNAME:     top level name (if empty, variable name will be used)
 %   PRINTCONTENTS:  flag to print field values/contents as well
 %   MAXARRAYLENGTH: for fields with array data, max length of values to
 %   print. Values of a 2-D (m,n) array are printed if the number of
 %   elements (m x n) is smaller or equal to maxarraylength. This is ignored
-%   if printvalues is 0. 
+%   if printvalues is 0.
 % _______________________________
 % EXAMPLES
-% 
-%   pS = printstruct(S, 'maxarray', 100); 
+%
+%   pS = printstruct(S, 'maxarray', 100);
 %   printstruct(S, 'nlevels', 2, 'printcontents', 0, 'nindent', 3)
-% 
+%
 
 % ---------------------- Copyright (C) 2015 Bob Spunt ----------------------
 %	Created:  2015-08-13
 %	Email:    spunt@caltech.edu
 % __________________________________________________________________________
-def = { ... 
+def = { ...
 	'nlevels',              -1,             ...
 	'printcontents',		 1,             ...
     'nindent',               0,             ...
     'structname',           '',             ...
-    'maxarraylength',       10              ...
+    'maxarraylength',       75              ...
 	};
 vals = setargs(def, varargin);
 if nargin==0, mfile_showhelp; fprintf('\t| - VARARGIN DEFAULTS - |\n'); disp(vals); return; end
+if iscell(S)
+    if all(cellfun(@isstruct, S))
+        fprintf(' - detected cell array of structs. printing iteratively...\n\n');
+    end
+else
+    S = {S};
+end
 if nlevels==0, nlevels = -1; end;
 if isempty(structname), structname = inputname(1); end
-if length(S)==1
-    str = recFieldPrint(S, nindent, nlevels, printcontents, maxarraylength, structname);
-    str = [cellstr(structname); str];
-else
-    str = []; 
-    for i = 1:length(S)
-        tmpstr = recFieldPrint(S(i), nindent, nlevels, printcontents, maxarraylength, structname);
-        str = [str; cellstr(sprintf('%s(%d)', structname, i)); tmpstr];
-        if i<length(S), str = [str; {' '}]; end
+pS = [];
+for i = 1:length(S)
+    cS = S{i};
+    if length(cS)==1
+        str = recFieldPrint(cS, nindent, nlevels, printcontents, maxarraylength, structname);
+        str = [cellstr(structname); str];
+    else
+        str = [];
+        for i = 1:length(cS)
+            tmpstr = recFieldPrint(cS(i), nindent, nlevels, printcontents, maxarraylength, structname);
+            str = [str; cellstr(sprintf('%s(%d)', structname, i)); tmpstr];
+            if i<length(cS), str = [str; {' '}]; end
+        end
     end
-end
-if nargout==0
-    for i = 1:length(str), disp(cell2mat(str(i, 1))); end
-else
-    pS = str;  
+    if nargout==0
+        for i = 1:length(str), disp(cell2mat(str(i, 1))); end
+    else
+        pS = [pS; str];
+    end
 end
 end
 % ==========================================================================
@@ -69,31 +80,31 @@ end
 % ==========================================================================
 function argstruct = setargs(defaults, optargs)
 % SETARGS Name/value parsing and assignment of varargin with default values
-% 
+%
 % This is a utility for setting the value of optional arguments to a
 % function. The first argument is required and should be a cell array of
 % "name, default value" pairs for all optional arguments. The second
 % argument is optional and should be a cell array of "name, custom value"
 % pairs for at least one of the optional arguments.
-% 
-%  USAGE: argstruct = setargs(defaults, args)  
+%
+%  USAGE: argstruct = setargs(defaults, args)
 % __________________________________________________________________________
 %  OUTPUT
-% 
+%
 % 	argstruct: structure containing the final argument values
 % __________________________________________________________________________
 %  INPUTS
-% 
-% 	defaults:  
+%
+% 	defaults:
 %       cell array of "name, default value" pairs for all optional arguments
-% 
-% 	optargs [optional]     
+%
+% 	optargs [optional]
 %       cell array of "name, custom value" pairs for at least one of the
-%       optional arguments. this will typically be the "varargin" array. 
+%       optional arguments. this will typically be the "varargin" array.
 % __________________________________________________________________________
 %  USAGE EXAMPLE (WITHIN FUNCTION)
-% 
-%     defaults    = {'arg1', 0, 'arg2', 'words', 'arg3', rand}; 
+%
+%     defaults    = {'arg1', 0, 'arg2', 'words', 'arg3', rand};
 %     argstruct   = setargs(defaults, varargin)
 %
 
@@ -101,7 +112,7 @@ function argstruct = setargs(defaults, optargs)
 % ---------------------- Copyright (C) 2015 Bob Spunt ----------------------
 %	Created:  2015-03-11
 %	Email:    spunt@caltech.edu
-% 
+%
 %   This program is free software: you can redistribute it and/or modify
 %   it under the terms of the GNU General Public License as published by
 %   the Free Software Foundation, either version 3 of the License, or (at
@@ -115,10 +126,10 @@ function argstruct = setargs(defaults, optargs)
 % __________________________________________________________________________
 if nargin < 1, mfile_showhelp; return; end
 if nargin < 2, optargs = []; end
-defaults = reshape(defaults, 2, length(defaults)/2)'; 
+defaults = reshape(defaults, 2, length(defaults)/2)';
 if ~isempty(optargs)
     if mod(length(optargs), 2)
-        error('Optional inputs must be entered as Name, Value pairs, e.g., myfunction(''name'', value)'); 
+        error('Optional inputs must be entered as Name, Value pairs, e.g., myfunction(''name'', value)');
     end
     arg = reshape(optargs, 2, length(optargs)/2)';
     for i = 1:size(arg,1)
@@ -129,7 +140,7 @@ if ~isempty(optargs)
            error('Input "%s" does not match a valid input.', arg{i,1});
        else
            defaults{idx,2} = arg{i,2};
-       end  
+       end
     end
 end
 for i = 1:size(defaults,1), assignin('caller', defaults{i,1}, defaults{i,2}); end
@@ -139,13 +150,13 @@ function mfile_showhelp(varargin)
 % MFILE_SHOWHELP
 ST = dbstack('-completenames');
 if isempty(ST), fprintf('\nYou must call this within a function\n\n'); return; end
-eval(sprintf('help %s', ST(2).file));  
+eval(sprintf('help %s', ST(2).file));
 end
 % | ADAPTED FROM FROM STRUCDISP
 function listStr    = recFieldPrint(Structure, indent, depth, printValues, maxArrayLength, structname)
 % RECFIELDPRINT Recursive printing of a structure variable
 %   This function and its dependencies were taken from:
-%       STRUCDISP.m by 
+%       STRUCDISP.m by
 %       Contact author: B. Roossien <roossien@ecn.nl>
 %       (c) ECN 2007-2008
 %       Version 1.3.0
@@ -157,7 +168,7 @@ if length(Structure) > 1
         varStr = createArraySize(Structure, structname);
         listStr = [{' '}; {[structname, varStr]}];
         body = recFieldPrint(Structure(1), indent, depth, ...
-                             printValues, maxArrayLength);            
+                             printValues, maxArrayLength);
         listStr = [listStr; body];
     else
         for iStruc = 1 : length(Structure)
@@ -176,10 +187,10 @@ strIndent   = getIndentation(indent + 1);
 listStr     = [listStr; {strIndent}];
 strIndent   = getIndentation(indent);
 for iField = 1 : length(strucFields)
-  
+
     fieldName = cell2mat(strucFields(iField));
     Field =  Structure.(fieldName);
-    
+
     % Empty structure
     if isempty(Field)
         strSize = createArraySize(Field, 'Structure');
@@ -197,7 +208,7 @@ for iField = 1 : length(strucFields)
         else
             listStr = [listStr; {line}];
         end
-    % Short vector structure of which the values should be printed    
+    % Short vector structure of which the values should be printed
     elseif (isvector(Field)) &&  ...
            (printValues > 0) && ...
            (length(Field) < maxArrayLength) && ...
@@ -224,8 +235,8 @@ for iField = 1 : length(strucFields)
         listStr = [listStr; {line}; lines];
     end
     % Some extra blank lines to increase readability
-    listStr = [listStr; {[strIndent '   |    ']}];   
-    
+    listStr = [listStr; {[strIndent '   |    ']}];
+
 end % End iField for-loop
 %% Field Filler
 % To properly align the field names, a filler is required. To know how long
@@ -291,8 +302,8 @@ otherFields   = fields(isOther == 1);
 % - Vectors are printed as long as they are shorter than maxArrayLength
 % - Matrices are printed if they have less elements than maxArrayLength
 % - The values of cells are not printed
-% Start with printing strings and characters. To avoid the display screen 
-% becoming a mess, the part of the string that is printed is limited to 31 
+% Start with printing strings and characters. To avoid the display screen
+% becoming a mess, the part of the string that is printed is limited to 31
 % characters. In the future this might become an optional parameter in this
 % function, but for now, it is placed in the code itself.
 % if the string is longer than 31 characters, only the first 31  characters
@@ -373,7 +384,7 @@ for iField = 1 : length(matrixFields)
         filler2 = char(ones(1, maxFieldLength + 6) * 32);
         dashes = char(ones(1, 12 * matrixSize(2) + 1) * 45);
         varCell = {[strIndent '   |' filler2 dashes]};
-        
+
         % first line with field name
         varStr = sprintf('%#10.2e |', Structure.(Field)(1, :));
         varCell = [varCell; {[strIndent '   |' filler ' ' ...
@@ -384,9 +395,9 @@ for iField = 1 : length(matrixFields)
             varCell = [varCell; {[strIndent '   |' filler2 '|' varStr]}];
         end
         varCell = [varCell; {[strIndent '   |' filler2 dashes]}];
-                    
+
     end
-    
+
     listStr = [listStr; varCell];
 end
 % Print cell array information, i.e. the size of the cell array. The
@@ -408,7 +419,7 @@ end
 function str = getIndentation(indent)
     x = '   |    ';
     str = '';
-    
+
     for i = 1 : indent
         str = cat(2, str, x);
     end
@@ -418,6 +429,6 @@ function varStr = createArraySize(varName, type)
 
     arraySizeStr = sprintf('%gx', varSize);
     arraySizeStr(length(arraySizeStr)) = [];
-    
+
     varStr = [' [' arraySizeStr ' ' type ']'];
 end
